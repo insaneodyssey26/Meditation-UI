bc13c6b556da3ce0a5380a80a2bf5158754304fe
insaneodyssey26 <hydrapikachu29@gmail.com>
2025-09-01 21:15:31 +0530

    Added required fonts and vector drawables

--- app/src/main/java/com/masum/meditationui/MainActivity.kt
+++ app/src/main/java/com/masum/meditationui/MainActivity.kt
@@ -11,16 +11,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.masum.meditationui.ui.theme.MeditationUITheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            MeditationUITheme {
 
-            }
         }
     }
 }
