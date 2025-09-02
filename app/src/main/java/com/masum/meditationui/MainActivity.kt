b9b91aeaaf9a739f1d8d5a62b200889cdaa5f63b
insaneodyssey26 <hydrapikachu29@gmail.com>
2025-09-02 18:17:37 +0530

    Chips section with LazyRow

--- app/src/main/java/com/masum/meditationui/MainActivity.kt
+++ app/src/main/java/com/masum/meditationui/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.masum.meditationui.ui.theme.ChipSection
 import com.masum.meditationui.ui.theme.HomeScreen
 
 class MainActivity : ComponentActivity() {
